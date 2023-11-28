9dbedb78fcff376a13995a1e480a94618948e49c
lucassantiagonahuel <lucassantiagonahuel@gmail.com>
2023-11-27 23:01:25 -0300

    Added challenge for documentation api

--- desafio34/src/dao/DTO/user.js
+++
@@ -1,8 +0,0 @@
-export default class UserDTO {
-  constructor(user) {
-    const userData = user.user;
-    (this.first_name = userData.user.first_name),
-      (this.last_name = userData.user.last_name),
-      (this.email = userData.user.email);
-  }
-}
--- segundaPreEntrega/src/public/js/products.js
+++
@@ -1,3 +0,0 @@
-const  addToCart = (productId)=> {
-    console.log('The product id :', productId);
-};
