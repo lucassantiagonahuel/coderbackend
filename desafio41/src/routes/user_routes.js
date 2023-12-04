6ab7f9840257d875045d205d701928f001e99c03
lucassantiagonahuel <lucassantiagonahuel@gmail.com>
2023-12-03 23:07:26 -0300

    Init challenge 41

---
+++ desafio41/src/routes/user_routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import userController from "../controllers/userController.js";
+import { authorization } from "../utils/auth.js";
+
+const router = Router();
+
+router.get("/:email", userController.getUserByEmail);
+
+router.post("/", userController.sendEmailResetPassword);
+
+router.put("/", userController.updatePassword);
+
+router.put("/premium/:uid",authorization(["admin"]),userController.updateRole);
+
+export default router;
---
+++ desafio41/src/service/userService.js
@@ -0,0 +1,45 @@
+import { UserClass } from "../dao/class/userClass.js";
+import { createHash, isValidPassword } from "../utils/utils.js";
+
+let userClass = new UserClass();
+
+const getUserByEmail = async (email) => {
+  const user = await userClass.getUserByEmail(email);
+  return user;
+};
+
+const updatePassword = async (user, newPassword) => {
+  if (isValidPassword(user, newPassword)) {
+    throw new Error("No puedes usar la misma contraseña");
+  }
+
+  const passwordUpdate = createHash(newPassword);
+  user.password = passwordUpdate;
+
+  const userUpdatePassword = await userClass.updateUserPassword(user);
+  return userUpdatePassword;
+};
+
+const getUserById = async (id) => {
+  const user = await userClass.getUserById(id);
+  return user;
+};
+
+const updateRole = async (user, role) => {
+  if (role === "user") {
+    user.role = "premium";
+  } else if (role === "premium") {
+    user.role = "user";
+  } else {
+    throw new Error("Error change password.");
+  }
+  const response = await userClass.updateUserRole(user);
+  return response;
+};
+
+export default {
+  getUserByEmail,
+  updatePassword,
+  getUserById,
+  updateRole,
+};
