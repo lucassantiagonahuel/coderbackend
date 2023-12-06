b96e790213b18fc907d6c9759875c1e044918c32
lucassantiagonahuel <lucassantiagonahuel@gmail.com>
2023-12-06 00:01:08 -0300

    Added challenge 41

--- desafio41/src/config/config.js
+++ desafio41/src/config/config.js
@@ -5,6 +5,7 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   mongoUrl: process.env.MONGO_URL,
+  mongoUrlTest: process.env.MONGO_URL_TEST,
   clientIdGithub: process.env.CLIENT_ID_GITHUB,
   clientSecretGithub: process.env.CLIENT_SECRET_GITHUB,
   callbackUrlGithub: process.env.CALLBACK_URL_GITHUB,
